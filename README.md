93554492021f745e4992d5eda54beee8ffc737f0
hxpdong <93167766+hxpdong@users.noreply.github.com>
2022-05-29 02:38:58 +0700

    upt ltw project

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # RentCar
+Dự án cho bài tập môn Lập Trình Web 
+  + Sử dụng PHP, HTML, CSS, JavaScript
+  + Cơ sở dữ liệu MySQL
---
+++ footer.php
@@ -0,0 +1,24 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="footer.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+    <div id="container-footer">
+        <div class="content-footer">
+            <h5>Performed by Ho Xuan Phuong Dong & Nguyen Duy Khang</h5>
+            <ul class="social-footer">
+                <li class="social-li-footer"><a href="https://www.facebook.com/hxpdong.01" class="fa fa-facebook" target="_blank"></a></li>
+                <li class="social-li-footer"><a href="https://github.com/ndkhangvl/RentCar" class="fa fa-github" target="_blank"></a></li>
+                <li class="social-li-footer"><a href="https://www.facebook.com/hxpdong.01" class="fa fa-google" target="_blank"></a></li>
+            </ul>
+            <hr></hr>
+            <p>hxpdong & ndkhangvl</p>
+        </div>
+    </div>
+</body>
+
+</html>
